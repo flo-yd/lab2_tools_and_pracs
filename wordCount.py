3739f8c861c4255f4df48f998b17b55d0d1d28c0
Padre108 <nonoyjpgerona@gmail.com>
2024-09-11 21:02:14 +0800

    added cellphone Nmber checker

--- wordCount.py
+++ wordCount.py
@@ -1,3 +1,9 @@
 def wordCount(word):
     return len(word)
 print(wordCount("Kabayo"))
+
+def cellphoneNumberchecker(number):
+    if len(number) == 11 and number.isdigit():
+     return True
+
+print(cellphoneNumberchecker(99055414862))
